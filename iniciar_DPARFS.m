7e2b3b631a7e80792a4b4f9929bf330c7e28e464
trini009 <119864174+trini009@users.noreply.github.com>
2022-12-06 10:45:40 -0300

    Add files via upload

---
+++ iniciar_DPARFS.m
@@ -0,0 +1,31 @@
+%% INICIAR TODO 
+
+%clc
+% close all 
+
+%cd DPABI_V6.2_220915/
+
+cd /home/triniibar/
+addpath spm12
+
+cd /home/triniibar/
+addpath dicm2
+
+cd /home/triniibar/
+addpath /home/triniibar/DPABI_V6.2_220915
+cd DPABI_V6.2_220915/
+addpath DPARSF
+addpath Analysis
+addpath DPABINet
+addpath DPABISurf
+addpath Logo
+addpath QualityControl
+addpath Standardization
+addpath StatisticalAnalysis
+addpath Subfunctions
+addpath Templates
+addpath Utilities
+addpath Viewer
+
+%dpabi
+
