978df33f08b0377d5160af59097f4108d852d5f1
Sol Cataldo <solcataldo98@gmail.com>
2023-03-03 16:53:19 -0300

    Read aparc.a2009s.stats and aparc.DKTatlas.stats

--- python/stat_to_csv.py
+++ python/stat_to_csv.py
@@ -43,7 +43,6 @@ def aparc_to_csv(file, name_new_file):
 
     return
 
-
 def brainvol_to_csv(file, name_new_file):
     '''Convert brainvol.stats to csv'''
 
@@ -69,8 +68,20 @@ def create_csvs(subject):
     subjects_dir_stats = "/home/sol/COVID/subjects/" + subject + "/stats"
 
     path_aseg = subjects_dir_stats + "/aseg.stats"
+
+    # aparc
     path_lh_aparc = subjects_dir_stats + "/lh.aparc.stats"
-    path_rh_aparc = subjects_dir_stats + "/lh.aparc.stats"
+    path_rh_aparc = subjects_dir_stats + "/rh.aparc.stats"
+
+    # aparca2009s
+    path_lh_aparca2009s = subjects_dir_stats + "/lh.aparc.a2009s.stats"
+    path_rh_aparca2009s = subjects_dir_stats + "/rh.aparc.a2009s.stats"
+
+    # aparcaDKT
+    path_lh_aparcDKT = subjects_dir_stats + "/lh.aparc.DKTatlas.stats"
+    path_rh_aparcDKT = subjects_dir_stats + "/rh.aparc.DKTatlas.stats"
+
+
     path_brainvol = subjects_dir_stats + "/brainvol.stats"
 
     subject_path = os.path.join("/home/sol/COVID/CSV_subjects/", subject)
@@ -79,10 +90,19 @@ def create_csvs(subject):
         os.makedirs(subject_path)
 
     common_path = "/home/sol/COVID/CSV_subjects/" + subject + '/' + subject
-
     aseg_to_csv(path_aseg, common_path + '_aseg.csv')
+    # aparc
     aparc_to_csv(path_lh_aparc, common_path + '_lh_aparc.csv')
     aparc_to_csv(path_rh_aparc, common_path + '_rh_aparc.csv')
+
+    # aparca2009s
+    aparc_to_csv(path_lh_aparca2009s, common_path + '_lh_aparca2009s.csv')
+    aparc_to_csv(path_rh_aparca2009s, common_path + '_rh_aparca2009s.csv')
+
+    # aparcDKT
+    aparc_to_csv(path_lh_aparcDKT, common_path + '_lh_aparcDKT.csv')
+    aparc_to_csv(path_rh_aparcDKT, common_path + '_rh_aparcDKT.csv')
+
     brainvol_to_csv(path_brainvol, common_path + '_brainvol.csv')
 
     return
