e7cef6c647846ac172f40a6712e31dc3a5246bbe
Martin Belzunce <martin.a.belzunce@gmail.com>
2023-05-16 11:03:10 -0300

    Add script to process the bold signals directly from the images and saves an animated gif with an overlay image of the atlas ROIs.

---
+++ matlab/SaveOverlayImageWithRois.m
@@ -0,0 +1,28 @@
+% mode = 0: meanFMRI
+% mode = 1:
+function SaveOverlayImageWithRois(fmri, atlas, filename, mode)
+    if ndims(fmri) == 4
+        meanFmri = mean(fmri,4);
+        meanFmri = (meanFmri-min(meanFmri, [], 'all'))./(max(meanFmri, [], 'all')+min(meanFmri, [], 'all'));
+    end
+    cm_jet = colormap('lines');
+    for j = 1 : size(meanFmri, 3)
+        % Go through each slice and generate a fused image:
+        numLabelsThisSlice = max(max(atlas(:,:,j)));
+        imageWithMasks = labeloverlay(1.5*meanFmri(:,:,j)', atlas(:,:,j)', 'Transparency',0.8, 'IncludedLabels',...
+            [1:numLabelsThisSlice], 'Colormap',cm_jet);
+        if ndims(imageWithMasks) == 3
+            [imind,cm] = rgb2ind(imageWithMasks,256); 
+        else
+            % Usually means that there are not any label
+            imind = imageWithMasks;
+            cm = colormap('gray');
+        end
+        % Write to the GIF File 
+        if j == 1 
+          imwrite(imind,cm,filename,'gif', 'Loopcount',inf); 
+        else 
+          imwrite(imind,cm,filename,'gif','WriteMode','append','DelayTime',0.1);
+        end
+        j = j + 1;
+    end
