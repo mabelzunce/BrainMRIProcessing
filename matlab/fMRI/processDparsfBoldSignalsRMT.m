dfd0bdc1565b8fa91bec7990c35014ba265451df
Martin Belzunce <martin.a.belzunce@gmail.com>
2023-11-23 19:05:09 -0300

    RMT analysis complete. Needs to include some visualization of the brai networks.

--- matlab/fMRI/processDparsfBoldSignalsRMT.m
+++ matlab/fMRI/processDparsfBoldSignalsRMT.m
@@ -20,7 +20,7 @@ end
 %% READ CSV
 subjectsInfo = readtable(csvFilename);
 
-%% READ ALL THE BOLD SIGNALS
+%% READ ALL THE BOLD SIGNALS, THE DEMOGRAPHICS DATA AND COMPUTE THE CORRELATION MATRICES
 roiSignalsDir = dir(roiSignalsPath);
 % Get filenames, removing dirs
 roiSignalsFilenames = {roiSignalsDir(~[roiSignalsDir.isdir]).name};
@@ -75,6 +75,23 @@ mmse = vec(mmse);
 ventricles = vec(ventricles);
 site = vec(categorical(site));
 scanner = vec(categorical(scanner));
+%% VIEW ALL THE CORRELATION MATRICES
+% AND SAVE IT IN AN ANIMATED GIF.
+filenameGif = [outputPath 'CorrelationMatrices'];
+figure;
+for i = 1 : numel(subjectNames)
+    him = imagesc(crossCorr(:,:,i));
+    %cm = him.Parent.Colormap; 
+    text(20,10,sprintf('Subject %d. Group %s', i, group(i)), 'FontSize', 14, 'FontWeight', 'bold', 'Color','w')
+    f=getframe(gca);
+    [im,cm] = rgb2ind(f.cdata,256); 
+    % Write to the GIF File 
+    if i == 1 
+      imwrite(im,cm,filenameGif,'gif', 'Loopcount',inf); 
+    else 
+      imwrite(im,cm,filenameGif,'gif','WriteMode','append','DelayTime',0.2);
+    end
+end
 %% LINEAR REGRESSION MODEL TO REGRESS OUT COVARIABLES
 variablesInTheRegression = {'Intercept','Age', 'Sex', 'Group'}; % In the same orhter as the variables are included in the regression.
 for i = 1 : size(crossCorr,1)
@@ -95,6 +112,7 @@ for i = 1 : size(crossCorr,1)
         crossCorrResiduals(i,j,:) = mdl.Coefficients.Estimate(1) + mdl.Residuals.Raw;
     end
 end
+
 %% CHECK THE MODEL
 alpha = 0.05;
 % p-values
@@ -119,16 +137,13 @@ for i = 1 : size(coefficients,3)
 end
 saveas(gca, [outputPath 'significantVariablesRegression'], 'png');
 %% COMPUTE MEAN VALUES FOR EACH RANDOM MATRIX
-meanCC = mean(crossCorrRegOut, 3);
-stdCC = std(crossCorrRegOut, 0, 3);
-meanCCRes = mean(crossCorrResiduals, 3);
+meanCC =  mean(crossCorrResiduals, 3); %mean(crossCorrRegOut, 3);
+stdCC = std(crossCorrResiduals, 0, 3);
 figure;
 set(gcf, 'Position', [100 100 2400 1200])
 t = tiledlayout('flow','TileSpacing','compact');
 nexttile
 imagesc(meanCC);
-nexttile
-imagesc(meanCCRes);
 saveas(gca, [outputPath 'meanCovMatrix'], 'png');
 %% FIND BRAIN NETWORKS USING RMT WITH THE MEAN CC MATRIX
 % 1) Get the eigenvalues and eigenvectores outside the Marchenk-Pastur
@@ -138,7 +153,7 @@ lambda = diag(lambda);
 % Ratio length/ROIs:
 N = size(meanCC,1);
 L = min(lengthSignals);
-Q=N/L;
+Q=L/N;
 % Std dev of the data:
 stdDevSignals = 1; % In theory, the singals are z normalised.
 % Probability Density Function
@@ -155,36 +170,52 @@ fprintf('Eigenvalues of the brain networks: '); disp(lambdaBrainNetworks');
 
 
 % Plot the distributions and eigenvalues.
-numBinsHist = 50;
-[histLambdas,lambdaBins] = hist(lambda,linspace(lambdaMin,lambdaMax,numBinsHist));
+numBinsHist = 100;
+[histLambdas,lambdaBins] = hist(abs(lambda),[linspace(0,lambdaMax,numBinsHist), [lambdaMax+1:max(lambda)]]);
 histLambdas=histLambdas/sum(histLambdas);     % Normalization la densidad
 % Theoretical pdf evaluated in the n points between lambda -a y lambda + b
+binsMP = linspace(lambdaMin,lambdaMax,numBinsHist);
 ft = @(lambda,a,b,c, s) (1./(2*pi*lambda*c*s^(2))).*sqrt((b-lambda).*(lambda-a));
-distMarchenkPastur = ft(lambdaBins, lambdaMin, lambdaMax, Q, stdDevSignals);
+distMarchenkPastur = ft(binsMP, lambdaMin, lambdaMax, Q, stdDevSignals);
 distMarchenkPastur = distMarchenkPastur./sum(distMarchenkPastur);
 % Plot Marchenko-Pastur
 figure;
-plot(lambdaBins, distMarchenkPastur);
+bar(lambdaBins, histLambdas);
 hold on;
-plot(abs(lambda), 0, 'x');
+plot(binsMP, distMarchenkPastur, 'LineWidth', 3);
+ylabel('P(lambda)')
+
+
+plot(abs(lambda), zeros(1, numel(lambda)), 'x', 'LineWidth', 3, 'MarkerSize',10);
+legend('Eigenvalues histogram', sprintf('Distr Marchenko-Pastur Q=N/L=%.1f',Q), 'Eigenvalues')
+saveas(gca, [outputPath 'eigenvalues'], 'png');
+xlim([0 10])
+saveas(gca, [outputPath 'eigenvaluesZoomed'], 'png');
 %%
 % 2) Get only the most important components for each brain network using
-% the IPR:
+% the IPR.
+brainNetworks = zeros(size(vBrainNetworks)); % Brain networks is the autovector with only the 1:IPR components.
 iprNetworks = round(1./sum(vBrainNetworks.^4));
 fprintf('IPR for each brain network: '); disp(iprNetworks);
 % Plot the contrbiutions:
 figure;
-for i = 1 : size(vBrainNetworks,2)
+for i = 1 : numBrainNetworks
     [sortedComponents, indexSortedComponents] = sort(abs(vBrainNetworks(:,i)), 'descend');
+    % Get ROIS of this brain networkL
+    roisBrainNetworks{i} = indexSortedComponents(1:iprNetworks(i));
+    % Define a brainnetowrk keeping the components of the autovector that
+    % contribute most using the IPR as threshold.
+    brainNetworks(roisBrainNetworks{i},i) = vBrainNetworks(roisBrainNetworks{i},i);
+    % Estimate the contribution:
     cumContribution = cumsum(sortedComponents)./sum(sortedComponents);
     cumContributionIpr(i) = cumContribution(iprNetworks(i));
     plot([1:numel(cumContribution)], cumContribution, ':', 'LineWidth', 2);
     hold on;
-    legendNetworks{i} = sprintf('Brain network %d (Eigenvalue=%.1f)', i, lambdaBrainNetworks(i));
+    legendNetworks{i} = sprintf('Brain network %d (Eigenvalue=%.1f, IPR=%d)', i, lambdaBrainNetworks(i), iprNetworks(i));
 end
 ylim([0 1])
-    % Plot the IPR
-for i = 1 : size(vBrainNetworks,2)
+% Plot the IPR
+for i = 1 : numBrainNetworks
     plot(iprNetworks(i), cumContributionIpr(i), 'xk', 'LineWidth', 2, 'MarkerSize', 10);
 end
 legend(legendNetworks, 'Location', 'SouthEast');
@@ -193,5 +224,22 @@ saveas(gca, [outputPath 'ComponentContributionsAndIPR'], 'png');
 %% SHOW BRAIN NETWORKS
 
 %% GET THE "STRENGTH OF SIGNAL" OF EACH BRAIN NETWORK
+% Whitening matrix
+W = diag(lambdaBrainNetworks.^-(1/2))*brainNetworks'; 
 
+% Apply the prwhitenning matrix:
+for i = 1 : numel(subjectNames)
+    S{i} = W*roiSignals{i}'; % We use a cell array just in case the signals don't have the same length for different subjects.
+    % Get the rms (normalized by number of time points):
+    for j = 1 : numBrainNetworks
+        rmsBrainNetowrksSubjects(i,j) = rms(S{i}(j,:))./size(S{1},2);
+    end
+end
+
+% Compare the rms between the two groups for each brain network using a
+% kruskal wallis statistical test
+for i = 1 : numBrainNetworks
+    p_kw(i) = kruskalwallis(rmsBrainNetowrksSubjects(:,i),group);
+    p_anova(i) = anova1(rmsBrainNetowrksSubjects(:,i),group);
+end
 
