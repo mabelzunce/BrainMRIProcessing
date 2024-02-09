a72594ac7bd05cdf21085803f51d170ccfb29eee
Martin Belzunce <martin.a.belzunce@gmail.com>
2024-02-09 18:49:46 -0300

    Functions to call Sienax from MATLAB.

---
+++ matlab/FSL/FslSienax.m
@@ -0,0 +1,23 @@
+% FSL Bet
+function out = FslSienax(filenameMri, outputSubdir, betOptions, ...
+    fastOptions, filenameLesionsMask)
+    if nargin == 4
+        lesionsMask = [];
+    end
+    [filepath,name,ext] = fileparts(filenameMri);
+    if contains(name, '.')
+        [p, name, ext2] = fileparts(name);
+    end
+    % Siena needs to run where the output
+    currentPath = pwd;
+    cd(filepath);
+    if isempty(fastOptions)
+        command = sprintf('sienax %s -o %s -B "%s"', name, ...
+            outputSubdir, betOptions);
+    else
+        command = sprintf('sienax %s -o %s -B "%s" -S "%s" -lm %s', name, ...
+            outputSubdir, betOptions, fastOptions, filenameLesionsMask);
+    end
+    out=system(command);
+    cd(currentPath)
+end
